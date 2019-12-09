6fb60e82f5b04a7b6742211380dbfad5c3273154
Adrian Igras <48068613+advvix@users.noreply.github.com>
2019-12-09 16:30:10 +0100

    Update lista 5 scena.py

--- lista 5 scena.py
+++ lista 5 scena.py
@@ -44,6 +44,7 @@ def paint():
     glutSwapBuffers()
     pass
 
+
 def perspective(fov, aspect, near, far):
     n, f = near, far
     t = np.tan((fov * np.pi / 180) / 2) * near
@@ -52,10 +53,11 @@ def perspective(fov, aspect, near, far):
     l = b * aspect
     assert abs(n - f) > 0
     return np.array((
-        ((2*n)/(r-l),           0,           0,  0),
-        (          0, (2*n)/(t-b),           0,  0),
-        ((r+l)/(r-l), (t+b)/(t-b), (f+n)/(n-f), -1),
-        (          0,           0, 2*f*n/(n-f),  0)))
+        ((2 * n) / (r - l), 0, 0, 0),
+        (0, (2 * n) / (t - b), 0, 0),
+        ((r + l) / (r - l), (t + b) / (t - b), (f + n) / (n - f), -1),
+        (0, 0, 2 * f * n / (n - f), 0)))
+
 
 def normalized(v):
     norm = np.linalg.norm(v)
@@ -77,15 +79,53 @@ def look_at(eye, target, up):
 
 def create_mvp(width, height):
     fov, near, far = 45, 0.1, 100
-    eye = np.array((4,3,3))
-    target, up = np.array((0,0,0)), np.array((0,1,0))
+    eye = np.array((4, 3, 3))
+    target, up = np.array((0, 0, 0)), np.array((0, 1, 0))
     projection = perspective(fov, width / height, near, far)
     view = look_at(eye, target, up)
     model = np.identity(4)
     mvp = model @ view @ projection
-    print(mvp)
-    return  mvp.astype(np.float32)
+    return mvp.astype(np.float32)
 
+
+def Cube(mousex, mousey, size):
+    global kej
+    glBegin(GL_QUADS)
+    glColor3f(0.5, 0, 0.5)
+    # upper right -> upper left -> lower left -> lower right
+    mousex = (windowWidth / 2 - mousex) / 130
+    mousey = -(windowHeight / 2 - mousey) / 20
+    """górna ściana"""
+    glVertex3f(mousex + size, mousey + size, 1.0 - size)  # front upper right
+    glVertex3f(mousex - size, mousey + size, 1.0 - size)  # front upper left
+    glVertex3f(mousex - size, mousey + size, 1.0 + size)  # back upper left
+    glVertex3f(mousex + size, mousey + size, 1.0 + size)  # back upper right
+    """tylnia ściana"""
+    glVertex3f(mousex - size, mousey + size, 1.0 + size)  # back upper left
+    glVertex3f(mousex + size, mousey + size, 1.0 + size)  # back upper right
+    glVertex3f(mousex + size, mousey - size, 1.0 + size)  # back lower right
+    glVertex3f(mousex - size, mousey - size, 1.0 + size)  # back lower left
+    """dolna ściana"""
+    glVertex3f(mousex + size, mousey - size, 1.0 + size)  # back lower right
+    glVertex3f(mousex - size, mousey - size, 1.0 + size)  # back lower left
+    glVertex3f(mousex - size, mousey - size, 1.0 - size)  # front lower left
+    glVertex3f(mousex + size, mousey - size, 1.0 - size)  # fron lower right
+    """przednia ściana"""
+    glVertex3f(mousex + size, mousey - size, 1.0 - size)  # front lower right
+    glVertex3f(mousex - size, mousey - size, 1.0 - size)  # front lower left
+    glVertex3f(mousex - size, mousey + size, 1.0 - size)  # front upper left
+    glVertex3f(mousex + size, mousey + size, 1.0 - size)  # front upper right
+    """lewa ściana"""
+    glVertex3f(mousex + size, mousey + size, 1.0 - size)  # front upper right
+    glVertex3f(mousex + size, mousey - size, 1.0 - size)  # front lower right
+    glVertex3f(mousex + size, mousey - size, 1.0 + size)  # back lower right
+    glVertex3f(mousex + size, mousey + size, 1.0 + size)  # back upper right
+    """prawa ściana"""
+    glVertex3f(mousex - size, mousey + size, 1.0 - size)  # front upper left
+    glVertex3f(mousex - size, mousey - size, 1.0 - size)  # front lower left
+    glVertex3f(mousex - size, mousey - size, 1.0 + size)  # back lower left
+    glVertex3f(mousex - size, mousey + size, 1.0 + size)  # back upper left
+    glEnd()
 
 
 # utworzenie okna
@@ -94,14 +134,86 @@ glutInitWindowPosition(int((ctypes.windll.user32.GetSystemMetrics(0) - windowWid
                        int((ctypes.windll.user32.GetSystemMetrics(1) - windowHeight) / 2))
 glutInitWindowSize(windowWidth, windowHeight)
 glutCreateWindow(b"PyOpenGL")
-# macierz punktów
-punkty = [-1.0, 0.0, 1.0,
-          1.0, 0.0, 1.0,
-          0.0, 1.0, 1.0]
+# # macierz punktów
+# punkty = [-1.0, 0.0, 1.0,
+#           1.0, 0.0, 1.0,
+#           0.0, 1.0, 1.0]
+
+
+punkty = [-1.0, -1.0, -1.0,
+          -1.0, -1.0, 1.0,
+          -1.0, 1.0, 1.0,
+          1.0, 1.0, -1.0,
+          -1.0, -1.0, -1.0,
+          -1.0, 1.0, -1.0,
+          1.0, -1.0, 1.0,
+          -1.0, -1.0, -1.0,
+          1.0, -1.0, -1.0,
+          1.0, 1.0, -1.0,
+          1.0, -1.0, -1.0,
+          -1.0, -1.0, -1.0,
+          -1.0, -1.0, -1.0,
+          -1.0, 1.0, 1.0,
+          -1.0, 1.0, -1.0,
+          1.0, -1.0, 1.0,
+          -1.0, -1.0, 1.0,
+          -1.0, -1.0, -1.0,
+          -1.0, 1.0, 1.0,
+          -1.0, -1.0, 1.0,
+          1.0, -1.0, 1.0,
+          1.0, 1.0, 1.0,
+          1.0, -1.0, -1.0,
+          1.0, 1.0, -1.0,
+          1.0, -1.0, -1.0,
+          1.0, 1.0, 1.0,
+          1.0, -1.0, 1.0,
+          1.0, 1.0, 1.0,
+          1.0, 1.0, -1.0,
+          -1.0, 1.0, -1.0,
+          1.0, 1.0, 1.0,
+          -1.0, 1.0, -1.0,
+          -1.0, 1.0, 1.0,
+          1.0, 1.0, 1.0,
+          -1.0, 1.0, 1.0,
+          1.0, -1.0, 1.0]
+
 zplus = 0.0
-kolory = [1.0, 0.0, 0.0,
-          0.0, 1.0, 0.0,
-          0.0, 0.0, 1.0]
+kolory = [0.583,  0.771,  0.014,
+    0.609,  0.115,  0.436,
+    0.327,  0.483,  0.844,
+    0.822,  0.569,  0.201,
+    0.435,  0.602,  0.223,
+    0.310,  0.747,  0.185,
+    0.597,  0.770,  0.761,
+    0.559,  0.436,  0.730,
+    0.359,  0.583,  0.152,
+    0.483,  0.596,  0.789,
+    0.559,  0.861,  0.639,
+    0.195,  0.548,  0.859,
+    0.014,  0.184,  0.576,
+    0.771,  0.328,  0.970,
+    0.406,  0.615,  0.116,
+    0.676,  0.977,  0.133,
+    0.971,  0.572,  0.833,
+    0.140,  0.616,  0.489,
+    0.997,  0.513,  0.064,
+    0.945,  0.719,  0.592,
+    0.543,  0.021,  0.978,
+    0.279,  0.317,  0.505,
+    0.167,  0.620,  0.077,
+    0.347,  0.857,  0.137,
+    0.055,  0.953,  0.042,
+    0.714,  0.505,  0.345,
+    0.783,  0.290,  0.734,
+    0.722,  0.645,  0.174,
+    0.302,  0.455,  0.848,
+    0.225,  0.587,  0.040,
+    0.517,  0.713,  0.338,
+    0.053,  0.959,  0.120,
+    0.393,  0.621,  0.362,
+    0.673,  0.211,  0.457,
+    0.820,  0.883,  0.371,
+    0.982,  0.099,  0.879]
 # shadery
 vs = compileShader(vsc, GL_VERTEX_SHADER)
 fs = compileShader(fsc, GL_FRAGMENT_SHADER)
@@ -119,14 +231,14 @@ while True:
     # modyfikacja trójkąta
     punkty[0] += 0.0001
     # model, widok, projekcja
-#    mvp = np.identity(4, float)
-    mvp = create_mvp(windowWidth,windowHeight)
+    #mvp = np.identity(4, float)
+    mvp = create_mvp(windowWidth, windowHeight)
     mvploc = glGetUniformLocation(sp, "mvp")  # pobieranie nazwy z shadera
     glUniformMatrix4fv(mvploc, 1, GL_FALSE, mvp)  # przekazywanie do shadera
     # ustawiamy pozycję i kolor
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, punkty)
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, kolory)
-    glDrawArrays(GL_TRIANGLES, 0, 3)
+    glDrawArrays(GL_TRIANGLES, 0, 3 * 12)
     glutSwapBuffers()
     glFlush()
     glutMainLoopEvent()
