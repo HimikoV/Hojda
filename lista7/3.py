2b622556538ac31989d7ec00fe1d833adfe08bde
HimikoV <48067467+HimikoV@users.noreply.github.com>
2020-01-13 16:24:07 +0100

    Update 3.py

--- lista7/3.py
+++ lista7/3.py
@@ -1,11 +1,11 @@
-#LISTA 7
-#ZAD 3
-#1.1...DONE
-#1.2...DONE
-#1.3...
-#1.4...
-#1.5...
-#1.6...
+# LISTA 7
+# ZAD 3
+# 1.1...DONE
+# 1.2...DONE
+# 1.3...
+# 1.4...
+# 1.5...
+# 1.6...
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
@@ -16,15 +16,17 @@ import numpy as np
 
 # licznik czasu - do wymuszenia częstotliwości odświeżania
 tick = 0
-eye = np.array([-5., 5., 10.])  # pozycja
+eye = np.array([-.3, 10.3, 23])  # pozycja
 orient = np.array([0., 0., -1.])  # kierunek
-up = np.array([0., 1., 0.]) # góra
+up = np.array([0., 1., 0.])  # góra
+
 
 # klasa pomocnicza, pozwalająca na odwoływanie się do słowników przez notację kropkową
 class dd(dict):
     __getattr__ = dict.get
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
+
 
 quadratic = gluNewQuadric()
 
@@ -40,7 +42,7 @@ part1.quad = quadratic
 part2 = {}
 part2 = dd(part2)
 part2.v = [-3, 0, 2]
-part2.p = [-5,1,10]
+part2.p = [-5, 1, 10]
 part2.m = 10
 part2.r = 1
 part2.col = [1, 0, 0.5]
@@ -49,26 +51,26 @@ part2.quad = quadratic
 part3 = {}
 part3 = dd(part3)
 part3.v = [4, 0, 4]
-part3.p = [-5,1,-5]
+part3.p = [-5, 1, -5]
 part3.m = 10
 part3.r = 1
 part3.col = [1, 1, 1]
 part3.quad = quadratic
 
-
-
 colors = np.array(
     [[1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1],
-    [0, 0, 0],
-    [1, 0, 1],
-    [1, 1, 0],
-    [0, 1, 1],
-    [1, 0.5, 0],
-    [0.5, 1, 0],
-    [0.5, 1, 0.5],
-    [0.5, 0, 0.5]])
+     [0, 1, 0],
+     [0, 0, 1],
+     [0, 0, 0],
+     [1, 0, 1],
+     [1, 1, 0],
+     [0, 1, 1],
+     [1, 0.5, 0],
+     [0.5, 1, 0],
+     [0.5, 1, 0.5],
+     [0.5, 0, 0.5]])
+
+
 # rysowanie sfery
 def drawSphere(part):
     glLoadIdentity()
@@ -76,11 +78,12 @@ def drawSphere(part):
     glColor3fv(part.col)
     gluSphere(part.quad, part.r, 16, 16)
 
+
 # rysowanie podłogi
 def drawFloor():
     glLoadIdentity()
     glColor3fv([0.0, 1.0, 0.0])
-    glPolygonMode(GL_FRONT,GL_FILL)
+    glPolygonMode(GL_FRONT, GL_FILL)
 
     glBegin(GL_POLYGON)
     glVertex3fv([-10, 0, -10])
@@ -91,9 +94,9 @@ def drawFloor():
 
 
 def drawScianki():
-    glLoadIdentity() # jedno sciano
+    glLoadIdentity()  # jedno sciano
     glColor3fv([0.3, 0.3, 0.3])
-    glPolygonMode(GL_FRONT,GL_LINE)
+    glPolygonMode(GL_FRONT, GL_LINE)
     glBegin(GL_POLYGON)
     glVertex3fv([-10, 0, -10])
     glVertex3fv([-10, 2, -10])
@@ -103,7 +106,7 @@ def drawScianki():
 
     glLoadIdentity()  # drugo sciano
     glColor3fv([0.3, 0.3, 0.3])
-    glPolygonMode(GL_FRONT,GL_LINE)
+    glPolygonMode(GL_FRONT, GL_LINE)
     glBegin(GL_POLYGON)
     glVertex3fv([-10, 0, 18])
     glVertex3fv([-10, 2, 18])
@@ -113,7 +116,7 @@ def drawScianki():
 
     glLoadIdentity()  # trzecio sciano
     glColor3fv([0.3, 0.3, 0.3])
-    glPolygonMode(GL_FRONT,GL_LINE)
+    glPolygonMode(GL_FRONT, GL_LINE)
     glBegin(GL_POLYGON)
     glVertex3fv([10, 0, 18])
     glVertex3fv([10, 2, 18])
@@ -123,7 +126,7 @@ def drawScianki():
 
     glLoadIdentity()  # czwarto sciano
     glColor3fv([0.3, 0.3, 0.3])
-    glPolygonMode(GL_FRONT,GL_LINE)
+    glPolygonMode(GL_FRONT, GL_LINE)
     glBegin(GL_POLYGON)
     glVertex3fv([10, 0, -10])
     glVertex3fv([10, 2, -10])
@@ -132,39 +135,37 @@ def drawScianki():
     glEnd()
 
 
-
-
 # ruch sfery
-def updateSphere(part, dt,aero,graw):
+def updateSphere(part, dt, aero, graw):
     # tutaj trzeba dodać obsługę sił, w tym grawitacji
-    part.v[0]=part.v[0]+aero[0]
-   # part.v[1] = part.v[1] + aero[1] - graw
+    part.v[0] = part.v[0] + aero[0]
+    part.v[1] = part.v[1] + aero[1] - graw
     part.v[2] = part.v[2] + aero[2]
     part.p[0] += dt * part.v[0]
     part.p[1] += dt * part.v[1]
     part.p[2] += dt * part.v[2]
 
 
-def checkSphereToSciankiCollision(part,k):
+def checkSphereToSciankiCollision(part, k):
     if part.p[0] - part.r < 7:
         pass
     else:
-        part.v[0] = -part.v[0]*k
+        part.v[0] = -part.v[0] * k
 
     if part.p[0] + part.r > -7:
         pass
     else:
-        part.v[0] = -part.v[0]*k
+        part.v[0] = -part.v[0] * k
 
     if part.p[2] - part.r < 15:
         pass
     else:
-        part.v[2] = - part.v[2]*k
+        part.v[2] = - part.v[2] * k
 
     if part.p[2] + part.r > -7:
         pass
     else:
-        part.v[2] = - part.v[2]*k
+        part.v[2] = - part.v[2] * k
 
 
 # sprawdzenie czy doszło do kolizji
@@ -180,7 +181,7 @@ def checkSphereToFloorCollision(part):
 
 # obsługa kolizji
 def updateSphereCollision(part):
-    if not checkSphereToFloorCollision(part1):
+    if not checkSphereToFloorCollision(part):
         return
     else:
         # jeśli sfera zachodzi pod podłogę, to podnieś ją
@@ -189,16 +190,34 @@ def updateSphereCollision(part):
         part.v[1] = - part.v[1]
 
 
-def aerodynamika(v,c):
+def aerodynamika(v, c):
     v = np.array(v)
-    vnorm=v/np.linalg.norm(v)
-    opor=v@v*c*vnorm*-1/2
+    vnorm = v / np.linalg.norm(v)
+    opor = v @ v * c * vnorm * -1 / 2
     opor.tolist()
     return opor
 
-def gravity(m,g):
-    grawitacja=m*g
+
+def gravity(m, g):
+    grawitacja = m * g
     return grawitacja
+
+
+def colisionOvO(part1, part2):
+    x = part1.v
+    y = part2.v
+    list1=[part1.p[0],part1.p[2]]
+    list2=[part2.p[0],part2.p[2]]
+    if np.sqrt((list1[0] - list2[0]) ** 2 + (list1[1] - list2[1]) ** 2) < 2 * part1.r:
+        part1.p[0], part1.p[2] = list1[0], list1[1]
+        part2.p[0], part2.p[2] = list2[0], list2[1]
+        part1.v = y
+        part2.v = x
+
+
+    else:
+        pass
+
 
 # wymuszenie częstotliwości odświeżania
 def cupdate():
@@ -208,17 +227,21 @@ def cupdate():
         return False
     tick = ltime
     return True
-k=0.94
-c=0.005
-g=0.005
+
+
+k = 0.95
+c = 0.02
+g = 0.01
 import random
-def keyboard(bkey,x,y):
+
+
+def keyboard(bkey, x, y):
     key = bkey.decode("utf-8")
-    global k,eye, orient, up,part1,c,g
+    global k, eye, orient, up, part1, c, g
     if key == 'k':
-        k+=0.05
-    if key=='l':
-        k-=0.05
+        k += 0.05
+    if key == 'l':
+        k -= 0.05
     if key == "e":
         eye = eye + orient * np.array([0.1, 0.1, 0.1]);
     if key == "q":
@@ -265,51 +288,56 @@ def keyboard(bkey,x,y):
         up = np.matmul(rot, np.array([0, 1, 0]));
         up = np.matmul(inverse, up);
     if key == 'y':
-        if random.random()>.5:
-            part1.v[0]+=15
-            part2.v[0] += 15
-            part3.v[0] += 15
-
+        x=random.random()
+        if x > .67:
+            part1.v[0] += random.randint(-30,30)
+            part1.v[2] += random.randint(-30,30)
+        elif x <.33:
+            part2.v[0] += random.randint(-30,30)
+            part2.v[2] += random.randint(-30,30)
         else:
-            part1.v[2]+=15
-            part2.v[2] += 15
-            part3.v[2] += 15
-    if key=='c':
-        c+=0.001
-    if key=='x':
-        c-=0.001
-    if key=='g':
-        g+=0.001
-    if key=='h':
-        g-=0.001
+            part3.v[0] += random.randint(-30,30)
+            part3.v[2] += random.randint(-30,30)
+    if key == 'c':
+        c += 0.001
+    if key == 'x':
+        c -= 0.001
+    if key == 'g':
+        g += 0.001
+    if key == 'h':
+        g -= 0.001
+
+
 # pętla wyświetlająca
 def display():
     if not cupdate():
         return
-    global part1,k,eye,orient,up,c,g,part2,part3
-    #print("współczynnik aerodynamiczny:", c)
+    global part1, k, eye, orient, up, c, g, part2, part3
+    print("współczynnik aerodynamiczny:", c)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     glFrustum(-1, 1, -1, 1, 1, 100)
     center = eye + orient
-    gluLookAt(eye[0],eye[1],eye[2],center[0],center[1],center[2],up[0],up[1],up[2])
+    gluLookAt(eye[0], eye[1], eye[2], center[0], center[1], center[2], up[0], up[1], up[2])
     glMatrixMode(GL_MODELVIEW)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
     drawScianki()
     drawFloor()
-    checkSphereToSciankiCollision(part1,k)
+    checkSphereToSciankiCollision(part1, k)
     checkSphereToSciankiCollision(part2, k)
     checkSphereToSciankiCollision(part3, k)
-    #print("współczynnik sprężystości: ", k)
-    #print("siła grawitacji: ",g)
-    updateSphere(part1, 0.1,aerodynamika(part1.v,c),gravity(part1.m,g))
+    print("współczynnik sprężystości: ", k)
+    print("siła grawitacji: ", g)
+    updateSphere(part1, 0.1, aerodynamika(part1.v, c), gravity(part1.m, g))
     updateSphereCollision(part1)
-    updateSphere(part2, 0.1,aerodynamika(part2.v,c),gravity(part2.m,g))
+    updateSphere(part2, 0.1, aerodynamika(part2.v, c), gravity(part2.m, g))
     updateSphereCollision(part2)
-    updateSphere(part3, 0.1,aerodynamika(part3.v,c),gravity(part3.m,g))
+    updateSphere(part3, 0.1, aerodynamika(part3.v, c), gravity(part3.m, g))
     updateSphereCollision(part3)
-
+    colisionOvO(part1, part2)
+    colisionOvO(part2, part3)
+    colisionOvO(part1, part3)
 
     drawSphere(part1)
     drawSphere(part2)
