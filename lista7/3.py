0df0feb1e3c4dffc80f0e2a7c65532b4bd6f4c3d
Adrian Igras <48068613+advvix@users.noreply.github.com>
2020-01-13 18:43:43 +0100

    Update 3.py

--- lista7/3.py
+++ lista7/3.py
@@ -10,6 +10,7 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
+
 from math import *
 import time
 import numpy as np
@@ -19,6 +20,10 @@ tick = 0
 eye = np.array([-.3, 10.3, 23])  # pozycja
 orient = np.array([0., 0., -1.])  # kierunek
 up = np.array([0., 1., 0.])  # góra
+windowWidth = 800
+windowHeight = 600
+mousex = windowWidth / 2
+mousey = windowHeight / 2
 
 
 # klasa pomocnicza, pozwalająca na odwoływanie się do słowników przez notację kropkową
@@ -71,13 +76,109 @@ colors = np.array(
      [0.5, 0, 0.5]])
 
 
+def mouseMotion(x, y):
+    global mousex, mousey
+    mousex = 0 if x < 0 else windowWidth if x > windowWidth else x
+    mousey = 0 if y < 0 else windowHeight if y > windowHeight else y
+    pass
+
+
+def mouseMouse(btn, stt, x, y):
+    pass
+
+
 # rysowanie sfery
 def drawSphere(part):
     glLoadIdentity()
+    glPolygonMode(GL_FRONT, GL_FILL)
+
     glTranslatef(part.p[0], part.p[1], part.p[2])
     glColor3fv(part.col)
     gluSphere(part.quad, part.r, 16, 16)
 
+
+def rotejszyn(part, mousex, mousey):
+    mousex = (windowWidth / 2 - mousex)
+    mousey = (windowHeight / 2 - mousey)
+    glTranslatef(part.p[0], part.p[1], part.p[2])
+    glRotatef(mousex, 0, 0.5, 0)
+    glTranslatef(-part.p[0], -part.p[1], -part.p[2])
+
+def hitAnimejszyn(part):
+    if metin2%2 == 0:
+       glTranslatef(0, 0, -4)
+    else:
+        glTranslatef(0, 0, 0)
+
+
+def kijekPrawdy(part, mousex, mousey):
+    glLoadIdentity()
+    rotejszyn(part, mousex, mousey)
+    hitAnimejszyn(part)
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+    glTranslatef(part.p[0], part.p[1], part.p[2] - 1.5)
+    gluSphere(part.quad, 0.3, 16, 16)
+
+
+    glLoadIdentity()
+    rotejszyn(part, mousex, mousey)
+    hitAnimejszyn(part)
+    glColor3fv([0.3, 0.3, 0.3])
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+    glBegin(GL_POLYGON)
+    glVertex3fv([part.p[0] - 0.3, part.p[1] + 0.3, part.p[2]- 1.5])
+    glVertex3fv([part.p[0] - 0.3, part.p[1] + 0.3, part.p[2] - 1.5])
+    glVertex3fv([part.p[0] + 0.3, part.p[1] + 0.3, part.p[2] - 1.5])
+    glVertex3fv([part.p[0] + 0.3, part.p[1] + 0.3, part.p[2]- 1.5])
+    glEnd()
+
+    glLoadIdentity()
+    rotejszyn(part, mousex, mousey)
+    hitAnimejszyn(part)
+    glColor3fv([0.3, 0.3, 0.3])
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+    glBegin(GL_POLYGON)
+    glVertex3fv([part.p[0] + 0.3, part.p[1] - 0.3, part.p[2]- 1.5])
+    glVertex3fv([part.p[0] + 0.3, part.p[1] - 0.3, part.p[2] - 20])
+    glVertex3fv([part.p[0] + 0.3, part.p[1] + 0.3, part.p[2] - 20])
+    glVertex3fv([part.p[0] + 0.3, part.p[1] + 0.3, part.p[2]- 1.5])
+    glEnd()
+
+    glLoadIdentity()
+    rotejszyn(part, mousex, mousey)
+    hitAnimejszyn(part)
+    glColor3fv([0.3, 0.3, 0.3])
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+    glBegin(GL_POLYGON)
+    glVertex3fv([part.p[0] - 0.3, part.p[1] - 0.3, part.p[2]- 1.5])
+    glVertex3fv([part.p[0] - 0.3, part.p[1] - 0.3, part.p[2] - 20])
+    glVertex3fv([part.p[0] - 0.3, part.p[1] + 0.3, part.p[2] - 20])
+    glVertex3fv([part.p[0] - 0.3, part.p[1] + 0.3, part.p[2]- 1.5])
+    glEnd()
+
+    glLoadIdentity()
+    rotejszyn(part, mousex, mousey)
+    hitAnimejszyn(part)
+    glColor3fv([0.3, 0.3, 0.3])
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+    glBegin(GL_POLYGON)
+    glVertex3fv([part.p[0] - 0.3, part.p[1] - 0.3, part.p[2]- 1.5])
+    glVertex3fv([part.p[0] - 0.3, part.p[1] - 0.3, part.p[2] - 20])
+    glVertex3fv([part.p[0] + 0.3, part.p[1] - 0.3, part.p[2] - 20])
+    glVertex3fv([part.p[0] + 0.3, part.p[1] - 0.3, part.p[2]- 1.5])
+    glEnd()
+
+    glLoadIdentity()
+    rotejszyn(part, mousex, mousey)
+    hitAnimejszyn(part)
+    glColor3fv([0.3, 0.3, 0.3])
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+    glBegin(GL_POLYGON)
+    glVertex3fv([part.p[0] - 0.3, part.p[1] - 0.3, part.p[2] - 20])
+    glVertex3fv([part.p[0] - 0.3, part.p[1] + 0.3, part.p[2] - 20])
+    glVertex3fv([part.p[0] + 0.3, part.p[1] + 0.3, part.p[2] - 20])
+    glVertex3fv([part.p[0] + 0.3, part.p[1] - 0.3, part.p[2] - 20])
+    glEnd()
 
 # rysowanie podłogi
 def drawFloor():
@@ -150,13 +251,13 @@ def checkSphereToSciankiCollision(part, k):
     if part.p[0] - part.r < 7:
         pass
     else:
-        part.p[0] = 7+part.r
+        part.p[0] = 7 + part.r
         part.v[0] = -part.v[0] * k
 
     if part.p[0] + part.r > -7:
         pass
     else:
-        part.p[0] = -part.r -7
+        part.p[0] = -part.r - 7
         part.v[0] = -part.v[0] * k
 
     if part.p[2] - part.r < 15:
@@ -207,9 +308,9 @@ def gravity(m, g):
     return grawitacja
 
 
-def colisionOvO(p1,p2):
-    list1=[p1.p[0],p1.p[2]]
-    list2=[p2.p[0],p2.p[2]]
+def colisionOvO(p1, p2):
+    list1 = [p1.p[0], p1.p[2]]
+    list2 = [p2.p[0], p2.p[2]]
     if np.sqrt((list1[0] - list2[0]) ** 2 + (list1[1] - list2[1]) ** 2) < 2 * part1.r:
         p1.p[0], p2.p[2] = list1[0], list1[1]
         p2.p[0], p2.p[2] = list2[0], list2[1]
@@ -241,35 +342,72 @@ def cupdate():
 k = 0.8
 c = 0.02
 g = 0.5
-rot_cam=0
-cam_r=15
 import random
+
+metin2 = 1
 def keyboard(bkey, x, y):
     key = bkey.decode("utf-8")
-    global k, rot_cam, cam_r, part1, c, g
+    global k, eye, orient, up, part1, c, g,metin2
     if key == 'k':
         k += 0.05
     if key == 'l':
         k -= 0.05
     if key == "e":
-        cam_r -= 1
+        eye = eye + orient * np.array([0.1, 0.1, 0.1]);
     if key == "q":
-        cam_r += 1
-    if key == 'd':
-        rot_cam += 5
-    if key == 'a':
-        rot_cam -= 5
+        eye = eye - orient * np.array([0.1, 0.1, 0.1]);
+    if key == "a":
+        right = np.cross(up, orient);
+        right = right / np.linalg.norm(right);
+        inverse = np.array([right, up, orient]);
+        inverse = np.transpose(inverse);
+        rot = np.array([[np.cos(0.1), 0, np.sin(0.1)], [0, 1, 0],
+                        [-np.sin(0.1), 0, np.cos(0.1)]]);
+        orient = np.matmul(rot, np.array([0, 0, 1]));
+        orient = np.matmul(inverse, orient);
+    if key == "d":
+        right = np.cross(up, orient);
+        right = right / np.linalg.norm(right);
+        inverse = np.array([right, up, orient]);
+        inverse = np.transpose(inverse);
+        rot = np.array([[np.cos(-0.1), 0, np.sin(-0.1)], [0, 1, 0],
+                        [-np.sin(-0.1), 0, np.cos(-0.1)]]);
+        orient = np.matmul(rot, np.array([0, 0, 1]));
+        orient = np.matmul(inverse, orient);
+
+    if key == "s":
+        right = np.cross(up, orient);
+        right = right / np.linalg.norm(right);
+        inverse = np.array([right, up, orient]);
+        inverse = np.transpose(inverse);
+        rot = np.array([[1, 0, 0], [0, np.cos(0.1), -np.sin(0.1)],
+                        [0, np.sin(0.1), np.cos(0.1)]]);
+        orient = np.matmul(rot, np.array([0, 0, 1]));
+        orient = np.matmul(inverse, orient);
+        up = np.matmul(rot, np.array([0, 1, 0]));
+        up = np.matmul(inverse, up);
+    if key == "w":
+        right = np.cross(up, orient);
+        right = right / np.linalg.norm(right);
+        inverse = np.array([right, up, orient]);
+        inverse = np.transpose(inverse);
+        rot = np.array([[1, 0, 0], [0, np.cos(-0.1), -np.sin(-0.1)],
+                        [0, np.sin(-0.1), np.cos(-0.1)]]);
+        orient = np.matmul(rot, np.array([0, 0, 1]));
+        orient = np.matmul(inverse, orient);
+        up = np.matmul(rot, np.array([0, 1, 0]));
+        up = np.matmul(inverse, up);
     if key == 'y':
-        x=random.random()
+        x = random.random()
         if x > .67:
-            part1.v[0] += random.randint(-30,30)
-            part1.v[2] += random.randint(-30,30)
-        elif x <.33:
-            part2.v[0] += random.randint(-30,30)
-            part2.v[2] += random.randint(-30,30)
+            part1.v[0] += random.randint(-30, 30)
+            part1.v[2] += random.randint(-30, 30)
+        elif x < .33:
+            part2.v[0] += random.randint(-30, 30)
+            part2.v[2] += random.randint(-30, 30)
         else:
-            part3.v[0] += random.randint(-30,30)
-            part3.v[2] += random.randint(-30,30)
+            part3.v[0] += random.randint(-30, 30)
+            part3.v[2] += random.randint(-30, 30)
     if key == 'c':
         c += 0.001
     if key == 'x':
@@ -278,20 +416,22 @@ def keyboard(bkey, x, y):
         g += 0.001
     if key == 'h':
         g -= 0.001
+    if key == '\x20':
+        metin2 += 1
+
 
 
 # pętla wyświetlająca
 def display():
     if not cupdate():
         return
-    global part1,k,rot_cam,cam_r,c,g
-    #print("współczynnik aerodynamiczny:", c)
+    global part1, k, eye, orient, up, c, g, part2, part3, x
+    # print("współczynnik aerodynamiczny:", c)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     glFrustum(-1, 1, -1, 1, 1, 100)
-    camx=sin(np.radians(rot_cam))*cam_r
-    camz=cos(np.radians(rot_cam))*cam_r
-    gluLookAt(camx,12,camz,0,0,0,0,1,0)
+    center = eye + orient
+    gluLookAt(eye[0], eye[1], eye[2], center[0], center[1], center[2], up[0], up[1], up[2])
     glMatrixMode(GL_MODELVIEW)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
@@ -300,8 +440,8 @@ def display():
     checkSphereToSciankiCollision(part1, k)
     checkSphereToSciankiCollision(part2, k)
     checkSphereToSciankiCollision(part3, k)
-    print("współczynnik sprężystości: ", k)
-    print("siła grawitacji: ", g)
+    # print("współczynnik sprężystości: ", k)
+    # print("siła grawitacji: ", g)
     updateSphere(part1, 0.1, aerodynamika(part1.v, c), gravity(part1.m, g))
     updateSphereCollision(part1)
     updateSphere(part2, 0.1, aerodynamika(part2.v, c), gravity(part2.m, g))
@@ -311,20 +451,23 @@ def display():
     colisionOvO(part1, part2)
     colisionOvO(part2, part3)
     colisionOvO(part1, part3)
-
+    kijekPrawdy(part3, mousex, mousey)
     drawSphere(part1)
     drawSphere(part2)
     drawSphere(part3)
+
     glutKeyboardFunc(keyboard)
     glFlush()
 
 
 glutInit()
-glutInitWindowSize(600, 600)
+glutInitWindowSize(800, 600)
 glutInitWindowPosition(0, 0)
 glutCreateWindow(b"Kolizje 05")
 glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH)
 glutDisplayFunc(display)
+glutMouseFunc(mouseMouse)
+glutMotionFunc(mouseMotion)
 glutIdleFunc(display)
 glClearColor(1.0, 1.0, 1.0, 1.0)
 glClearDepth(1.0)
